c7fea11f7f7a48b8cf4f7c5b1ec2569440b31c1a
kevinsmithseven <kevinsmithseven@gmail.com>
2023-10-01 11:04:25 -0500

    added functionality to display current weather info on page based on user input

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,12 @@
 // Global Variables
 var APIKey = "ac447bae7925ca40d90866afd750b493";
+var currentDay = dayjs().format('MM/DD/YYYY');
 var userCity = document.querySelector("#search-form");
+var searchHistory = document.querySelector("#search-history")
+var currWXContainer = document.querySelector("#current-container");
+var fiveDayContainer = document.querySelector("#five-day-container");
+
+
 
 
 // Handles click or enter press by user and checks if City exists
@@ -13,7 +19,7 @@ function formSubmitHandler(event) {
         getCurrWX(cityInput);
 
         // Add city to local storage and add clickable button
-        
+
     } else {
         alert("Please enter a valid city name");
     }
@@ -31,10 +37,18 @@ function getCurrWX(cityInput) {
                 throw new Error("City not found");
             }
         })
-        .then(function (currWX) {
+        .then(function (currWXdata) {
             //display data logic
-            console.log(currWX);
+            console.log(currWXdata);
+
+
+            displayCurrWX(currWXdata);
             //need to grab long and lat and do another fetch to get 5 Day forecast
+            var lat = currWXdata.coord.lat;
+            var lon = currWXdata.coord.lon;
+            console.log(lat);
+            console.log(lon);
+
 
         })
         .catch(function (error) {
@@ -42,6 +56,31 @@ function getCurrWX(cityInput) {
         });
 }
 
+function displayCurrWX(currWXdata) {
+    
+    var currWXCityDate = document.createElement("h3");
+    var currWXTemp = document.createElement("p");
+    var currWXWind = document.createElement("p");
+    var currWXHum = document.createElement("p");
+    currWXContainer.textContent = "";
+    currWXCityDate.textContent = currWXdata.name + "  (" + currentDay + ")";
+    currWXTemp.textContent = "Temp: " + currWXdata.main.temp + " °F";
+    currWXWind.textContent = "Wind: " + currWXdata.wind.speed + " MPH";
+    currWXHum.textContent = "Humidity: " + currWXdata.main.humidity + "%";
+    currWXContainer.append(currWXCityDate);
+    currWXContainer.append(currWXTemp);
+    currWXContainer.append(currWXWind);
+    currWXContainer.append(currWXHum);
+}
+
+
+ 
+  
+
+
+
 userCity.addEventListener('submit', formSubmitHandler);
 console.log(userCity);
+
+console.log(currentDay);
 
